c67ba4381ce9419dbfc4d07297147a0dd6f66a1c
ucg8j <lukesingham@gmail.com>
2021-04-24 21:41:22 +0100

    Prints offending lines with whitespace

--- cmd/lint.go
+++ cmd/lint.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"bufio"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -15,7 +18,31 @@ var lintCommand = &cobra.Command{
 	Short: "Lints the SQL",
 	Args:  cobra.ExactValidArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("hello lint")
+		// trimWhiteSpace(file)
+		lines := LinesInFile(args[0])
+
+		offendingLines := map[int][]string{}
+
+		for index, line := range lines  {
+			newline := strings.TrimRight(line, " ")
+
+			if line != newline {
+				offendingLines[index] = append(offendingLines[index], "Trailing whitespace")
+			}
+		}
+		for index, line := range offendingLines {
+			fmt.Printf("line %v, issue = %v\n", index , line)
+		}
 	},
-	// ValidArgsFunction: completeModelFn,
+}
+
+func LinesInFile(filename string) []string  {
+	f, _ := os.Open(filename)
+	scanner := bufio.NewScanner(f)
+	result := []string{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		result = append(result, line)
+	}
+	return result
 }
