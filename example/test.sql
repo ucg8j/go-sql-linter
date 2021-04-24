43bf6524dc05ec00a7966a274d6b0b950aa35d33
ucg8j <lukesingham@gmail.com>
2021-04-24 09:21:13 +0100

    Create test.sql

---
+++ example/test.sql
@@ -0,0 +1,25 @@
+WITH bad_cte AS (
+  SeLeCT 
+    a,   
+    b,  
+
+
+  from `project.dataset.table` a
+),
+
+good_cte AS (
+  SELECT
+  a,
+  c,
+  d
+
+  FROM `project.dataset.table` b
+)
+
+SELECT
+a,
+b,
+c,
+d,
+FROM bad_cte
+LEFT JOIN good_cte USING (a)
