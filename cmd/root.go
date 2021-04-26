544e428a7f6398a5bf9c7271c4d7513b9c21feb8
ucg8j <lukesingham@gmail.com>
2021-04-26 21:30:51 +0100

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -8,20 +8,17 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-  Use:   "gsl",
-  Short: "",
-  Long: `A Fast and Flexible Static Site Generator built with
-                love by spf13 and friends in Go.
-                Complete documentation is available at http://hugo.spf13.com`,
-  Run: func(cmd *cobra.Command, args []string) {
-    // Do Stuff Here
+	Use:   "gsl",
+	Short: "A SQL linter written in go",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Hello, world")
-  },
+	},
 }
 
 func Execute() {
-  if err := rootCmd.Execute(); err != nil {
-    fmt.Fprintln(os.Stderr, err)
-    os.Exit(1)
-  }
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
